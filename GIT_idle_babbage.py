4b32db6ca9cfabd2b6b56f6f3cddc817b645d9c0
Coley Stillman <c05stil@siena.eduoofoof5!qyesgit config --global user.name Coley>
2015-12-08 15:53:03 -0500

    Babbage code

---
+++ GIT_idle_babbage.py
@@ -0,0 +1,11 @@
+def statistics(grades1.csv):
+    x = grades1.csv
+    for i in x:
+        print x
+
+
+
+
+
+    
+    
---
+++ babbage_code.py
@@ -0,0 +1,14 @@
+def statistics(grade):
+    x = grade
+    m = mean[x]
+    s = std[x]
+    h = max[x]
+    l = min[x]
+    return m,s,h,l
+
+
+
+
+
+    
+    
